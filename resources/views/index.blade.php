a6fd8ecf8048d50ea54b5bbeb9b7d5b450b920e6
David Tang <david.tang@verizondigitalmedia.com>
2019-02-17 15:28:50 -0800

    change title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <title>SQL Playground</title>
+  <title>Try SQL</title>
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
