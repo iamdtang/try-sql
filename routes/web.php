d2ab2ad6937e7e7a65f94611e20d87a5448ec855
David Tang <david.tang@verizondigitalmedia.com>
2019-02-25 19:21:25 -0800

    adds exercises

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
   return view('index');
 });
 
+Route::get('/exercises', function () {
+  return view('exercises');
+});
+
 Route::post('/query', function (Request $request) {
   $results = DB::select($request->input('sql'));
   return response()->json($results);
